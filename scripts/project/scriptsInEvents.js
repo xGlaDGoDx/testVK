ff2ffbb2b3b64f90368f1361252c0ff1eb17c9ca
GlaDGoD <96576092+xGlaDGoDx@users.noreply.github.com>
2023-07-30 18:47:44 +0500

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,33 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event2_Act1(runtime, localVars)
+	{
+		var path = document.createElement('script');
+		path.src = '//vk.com/js/api/openapi.js';
+		document.head.appendChild(path);
+		
+		path.onload = function() {
+			console.log(123);
+			
+			VK.init({
+				apiId: 51693034
+			});
+			
+			VK.api("account.getProfileInfo", function (data) {
+		    	alert("Info: " + data.first_name + " " + data.last_name + " " + data.id);
+		    	console.log("Info: " + 
+					data.first_name + " " + 
+					data.last_name + " " + 
+					data.id);
+		    	returnStr = data.id;
+			});
+		}
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
